c049fdade1f51f9352c728575d194f110a7f6256
karachid <r.kabbab@gmail.com>
2023-06-15 09:35:38 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Entry point of the prog
+ * Return: Always 1 (SUCCESS)
+ */
+
+int main()
+{
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}

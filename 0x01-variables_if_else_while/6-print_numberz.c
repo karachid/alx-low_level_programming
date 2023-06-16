098dec2d973ec58ac67aedf6114ec214ea496915
karachid <r.kabbab@gmail.com>
2023-06-16 10:13:24 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the prog
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

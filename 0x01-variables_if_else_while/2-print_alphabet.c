fec767431a4784833cf29528f6d369b09f9f686d
karachid <r.kabbab@gmail.com>
2023-06-16 09:49:16 -0700

    print alphabets in lowercase using putchar only twice

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the prog
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+int main(void)
+{
+	for (int c = 97; c < 123; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

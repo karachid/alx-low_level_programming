b77feb038ad23e9d64fa143d4de2986aa200e77c
karachid <r.kabbab@gmail.com>
2023-06-16 10:02:56 -0700

    printing all alphabets except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the prog
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 97; c < 123; c++)
+	{
+		if (c != 101 && c != 113)
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

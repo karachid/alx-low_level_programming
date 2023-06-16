9bea0dbc2cbf0c5b8f7ae807883072ef4df1c9cc
karachid <r.kabbab@gmail.com>
2023-06-16 10:21:54 -0700

    hexadecimal digits

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the prog
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+		putchar(c);
+	for (c = 97; c < 103; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

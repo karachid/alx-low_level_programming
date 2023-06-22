79a17d81f5799cae6463f971dddea68d6bf1bf39
karachid <r.kabbab@gmail.com>
2023-06-22 11:34:16 -0700

    new imple

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _isdigit - a function that checks if a parameter is a digit
- * @c : integer
+ * _isdigit - Entry Function
+ * @c: integer
  * Return: 1 if c is a digit, 0 otherwise
-*/
+ */
 int _isdigit(int c)
-{	
-	if (c >= 0 && c <= 9)
+{
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}

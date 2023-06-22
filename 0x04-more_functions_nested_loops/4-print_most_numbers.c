25caf70d0de65bae5feece4077979fa830df2ff5
karachid <r.kabbab@gmail.com>
2023-06-22 11:54:12 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints most numbers
+ * Return: void (NOTHING)
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}

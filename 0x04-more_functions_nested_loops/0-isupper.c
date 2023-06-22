ca37f8f0474ec22e0f7e5a92f9d7906554b4f782
karachid <r.kabbab@gmail.com>
2023-06-22 10:28:02 -0700

    creating a folder and doing the first task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+/**
+ * _isupper : checks for uppercase character
+ *
+ * Return: returns 1 if a character is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}

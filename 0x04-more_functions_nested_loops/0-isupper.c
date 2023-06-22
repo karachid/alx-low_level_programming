42d471fe4773ee05bb8fe8873103d92bc04b8c28
karachid <r.kabbab@gmail.com>
2023-06-22 10:40:38 -0700

    Adding comments

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,9 @@
+#include "main.h"
+
 /**
- * _isupper : checks for uppercase character
+ * _isupper - checks for uppercase character
+ *
+ * @c : an integer param
  *
  * Return: returns 1 if a character is uppercase, 0 otherwise
  */

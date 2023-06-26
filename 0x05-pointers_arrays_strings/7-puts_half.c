90bf8760d3fb9527cde140e3d7de342a78967851
karachid <r.kabbab@gmail.com>
2023-06-26 16:03:26 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @s : string to be used for printing
+ * Return: void (NOTHING)
+ */
+
+void puts_half(char *str)
+{
+	int c = 0;
+
+	while (str[c] != '\0')
+	{
+		c++;
+	}
+	if (c % 2 == 0)
+		c = c / 2;
+	else
+		c = (c - 1) / 2;
+	while(str[c] != '\0')
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
+}

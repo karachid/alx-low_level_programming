76c9806b0b38902c4b1681c12f748b1834274e4a
karachid <r.kabbab@gmail.com>
2023-06-26 14:05:12 -0700

    corrected betty style

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,20 +1,19 @@
 #include"main.h"
 
 /**
- * _strlen - counts how many chars a string contains
- *
- * @s : string to be used for counting the number of chars
- *
- * Return : int (NUMBER OF CHARS)
+ * _strlen - counts number of chars
+ * @s : string to be used for counting
+ * Return: count (NUMBER OF CHARS)
  */
+
 int _strlen(char *s)
 {
-        int count = 0;
+	int count = 0;
 
-        while(*s != '\0')
-        {
-                count++;
-                s++;
-        }
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
 	return (count);
 }

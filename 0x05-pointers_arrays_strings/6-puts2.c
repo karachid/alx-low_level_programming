f57965001e642574ced070bcb92800ae12d80c1a
karachid <r.kabbab@gmail.com>
2023-06-26 15:48:44 -0700

    6 puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints char such that its index mod 2 equals 0
+ *
+ * @str: char
+ *
+ * Return: void (NOTHING)
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i++]);
+		}
+	}
+	_putchar('\n');
+}

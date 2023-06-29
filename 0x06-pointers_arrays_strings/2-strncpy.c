e29cfb7e82ee67e09cb582dd4e15cf0216858e0d
karachid <r.kabbab@gmail.com>
2023-06-29 15:31:58 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * _strncpy - copies n chars of src into dest
+ * @dest: first argument
+ * @src: second argument
+ * @n: third param
+ * Return: copied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}

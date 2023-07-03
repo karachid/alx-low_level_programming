f84de524dde04eeacc77b818381619ddecdbc945
karachid <r.kabbab@gmail.com>
2023-07-03 15:59:01 -0700

    diag sum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  print_diagsums - prints diag sum
+ *  @a: input matrix
+ *  @size: size
+ *  Return: void (NOTHING)
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i * size + i];
+	}
+	for (i = size - 1; i >= 0; i--)
+	{
+		sum2 += a[i * size + (size - i - 1)];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}

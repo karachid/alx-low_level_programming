75666c63dce88aa5caa34749be0a3445fdf9eefc
karachid <r.kabbab@gmail.com>
2023-07-03 15:37:51 -0700

    logic changed

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include"main.h"
+#include<stddef.h>
 
 /**
  * _strchr - locates a char in a string
@@ -9,11 +10,17 @@
 
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
-
-	while (s[i] != '\0' && s[i] != c)
-		i++;
-	if (s[i] == '\0')
-		return (NULL);
-	return (s[i]);
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
 }

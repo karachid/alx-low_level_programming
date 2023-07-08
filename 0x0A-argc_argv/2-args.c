a1fa535bae85ce7dea24991672d932ff44cda67c
karachid <r.kabbab@gmail.com>
2023-07-07 18:20:26 -0700

    two args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+include <stdio.h>
+
+/**
+ * main - prints all arguments it receives.
+ * @argc: argument's number
+ * @argv: arguments values
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}

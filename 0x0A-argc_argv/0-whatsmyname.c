0485976b00cfe89d8155e4fba320e3b5464cafec
karachid <r.kabbab@gmail.com>
2023-07-07 18:14:26 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name followed by a new line
+ * @argc: argument's number
+ * @argv: arguments values
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

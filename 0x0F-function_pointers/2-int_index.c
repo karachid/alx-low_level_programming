f5c9a3c64ea4b1abb8342b233a79af78d25fa247
karachid <r.kabbab@gmail.com>
2023-07-19 10:58:33 -0700

    int index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,17 +11,16 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	size_t i;
-	int rvalue;
 
-	if (array && cmp)
+	if (size > 0)
 	{
-		if (size <= 0)
-			return (-1);
-
-		for (i = 0; i < size; i++)
+		if (array && cmp)
 		{
-			if (cmp(array[i]))
-				return (i);
+			for (i = 0; i < size; i++)
+			{
+				if (cmp(array[i]))
+					return (i);
+			}
 		}
 	}
 	return (-1);

5e0fcd1036ac075e1e65ac0759d647f17b065aa3
karachid <r.kabbab@gmail.com>
2023-07-27 10:44:33 -0700

    fixing some betty issuers

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,7 +18,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (newNode == NULL)
 		return (NULL);
-	
+
 	newNode->str = strdup(str);
 	newNode->len = strlen(str);
 	newNode->next = *head;

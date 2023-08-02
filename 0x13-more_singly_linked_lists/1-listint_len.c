b6832f16863b21c214ec5a2d24b4ee4efc067522
karachid <r.kabbab@gmail.com>
2023-08-01 17:24:43 -0700

    list int

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of items
+ * @h: head pointer
+ * Return: the number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t c = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		c++;
+	}
+	return (count);
+}

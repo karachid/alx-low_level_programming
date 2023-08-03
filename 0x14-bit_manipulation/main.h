1e6402d02b99a51523d97861c43a2898029e0934
karachid <r.kabbab@gmail.com>
2023-08-03 16:19:07 -0700

    including _putchar in header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 unsigned long int _power(unsigned int base, unsigned int pow);
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 

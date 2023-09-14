c5af86637999e91dc65f03f2aaa9f2eeeb9f8ae5
karachid <r.kabbab@gmail.com>
2023-09-14 12:51:13 -0700

    get node at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned i = 0;
+
+	while (head)
+	{
+		if (i == index)
+			return (head);
+		
+		head = head->next;
+		i++;
+	}
+	return (NULL);
+}

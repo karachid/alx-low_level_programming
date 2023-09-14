8a7d55a3a17183831ce0ce80b65074cbe309ca77
karachid <r.kabbab@gmail.com>
2023-09-14 13:58:07 -0700

    sum in list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -10,10 +10,10 @@ int sum_dlistint(dlistint_t *head)
 	dlistint_t *c = head;
 	int s = 0;
 
-	while (c)
+	while (c != NULL)
 	{
-		c = c->next;
 		s = s + c->n;
+		c = c->next;
 	}
 	return (s);
 }

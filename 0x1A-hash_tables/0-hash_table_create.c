686bf6b16bf4fa615e7864cef04bddcdb77b9113
Rachid KABBAB <r.kabbab@gmail.com>
2023-10-06 16:37:06 -0700

    0  create a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a new hash table
+ * @size: size of the hash table
+ * Return: (hash_table_t*) pointer to the created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table *ht = NULL;
+	unsigned short i = 0;
+
+	ht = malloc(sizeof(hash_table_s));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (!ht->array)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL
+
+	return (ht);
+}
